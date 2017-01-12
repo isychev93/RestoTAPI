ed0e7cfc4ec93a0532ce4ca64ed6c7d13dd50f45
ISychev <isychev@iiko.ru>
2017-01-12 18:16:29 +0300

    First commit.

---
+++ RestoATAPI/RestoTAPIMonitor/MainForm.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace RestoTAPIMonitor
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.lineColumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.callColumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.stateColumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.callerColumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.calledColumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.connectedColumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.buttonSubscribe = new System.Windows.Forms.Button();
+            this.buttonUnsubscribe = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.CheckOnClick = true;
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(12, 24);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(195, 409);
+            this.checkedListBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Lines:";
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.lineColumn,
+            this.callColumn,
+            this.stateColumn,
+            this.callerColumn,
+            this.calledColumn,
+            this.connectedColumn});
+            this.listView1.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(213, 24);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(740, 218);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // lineColumn
+            // 
+            this.lineColumn.Text = "Line";
+            this.lineColumn.Width = 91;
+            // 
+            // callColumn
+            // 
+            this.callColumn.Text = "CallId";
+            this.callColumn.Width = 56;
+            // 
+            // stateColumn
+            // 
+            this.stateColumn.Text = "State";
+            this.stateColumn.Width = 74;
+            // 
+            // callerColumn
+            // 
+            this.callerColumn.Text = "Caller";
+            this.callerColumn.Width = 99;
+            // 
+            // calledColumn
+            // 
+            this.calledColumn.Text = "Called";
+            this.calledColumn.Width = 93;
+            // 
+            // connectedColumn
+            // 
+            this.connectedColumn.Text = "Connected";
+            this.connectedColumn.Width = 81;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(210, 8);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Lines state:";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(135, 7);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(69, 17);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Select all";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // buttonSubscribe
+            // 
+            this.buttonSubscribe.Location = new System.Drawing.Point(12, 439);
+            this.buttonSubscribe.Name = "buttonSubscribe";
+            this.buttonSubscribe.Size = new System.Drawing.Size(91, 23);
+            this.buttonSubscribe.TabIndex = 6;
+            this.buttonSubscribe.Text = "Subscribe";
+            this.buttonSubscribe.UseVisualStyleBackColor = true;
+            this.buttonSubscribe.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // buttonUnsubscribe
+            // 
+            this.buttonUnsubscribe.Location = new System.Drawing.Point(109, 439);
+            this.buttonUnsubscribe.Name = "buttonUnsubscribe";
+            this.buttonUnsubscribe.Size = new System.Drawing.Size(95, 23);
+            this.buttonUnsubscribe.TabIndex = 7;
+            this.buttonUnsubscribe.Text = "Unsubscribe";
+            this.buttonUnsubscribe.UseVisualStyleBackColor = true;
+            this.buttonUnsubscribe.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(392, 436);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(151, 24);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Application state:";
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelStatus.Location = new System.Drawing.Point(577, 436);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(186, 24);
+            this.labelStatus.TabIndex = 9;
+            this.labelStatus.Text = "initialization (whait)";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(965, 474);
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.buttonUnsubscribe);
+            this.Controls.Add(this.buttonSubscribe);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkedListBox1);
+            this.Name = "MainForm";
+            this.Text = "RestoTAPITestTool";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader lineColumn;
+        private System.Windows.Forms.ColumnHeader callColumn;
+        private System.Windows.Forms.ColumnHeader stateColumn;
+        private System.Windows.Forms.ColumnHeader callerColumn;
+        private System.Windows.Forms.ColumnHeader calledColumn;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ColumnHeader connectedColumn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button buttonSubscribe;
+        private System.Windows.Forms.Button buttonUnsubscribe;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelStatus;
+    }
+}
---
+++ RestoATAPI/RestoTAPIMonitor/MainForm.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using JulMar.Atapi;
+
+namespace RestoTAPIMonitor
+{
+    public partial class MainForm : Form
+    {
+        private readonly object lockOnLog = new object();
+
+        private const int COLUMNS_LINE = 1;
+        private const int COLUMNS_CID = 2;
+        private const int COLUMNS_STATE = 3;
+        private const int COLUMNS_CALLER = 4;
+        private const int COLUMNS_CALLED = 5;
+        private const int COLUMNS_CONNECTED = 5;
+
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        private TapiLine[] availableTapiLines;
+        private TapiLine[] subscribedLines;
+
+        private TapiManager tapiManager = new TapiManager("RESTOTAPITestToolMonitor");
+        public MainForm()
+        {
+
+            InitializeComponent();
+            buttonSubscribe.Enabled = false;
+            buttonUnsubscribe.Enabled = false;
+
+            log.Info("Start programm");
+
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            log.Info("Try to initialize");
+
+            tapiManager.NewCall += TapiManagerOnNewCall;
+            tapiManager.CallInfoChanged += TapiManagerOnCallInfoChanged;
+            tapiManager.CallStateChanged += TapiManagerOnCallStateChanged;
+            var taskToInit = Task.Factory.StartNew(() =>
+            {
+                if (tapiManager.Initialize() == false)
+                {
+                    log.Info("Initialized failed.");
+                    ShowError();
+                }
+            });
+
+            taskToInit.ContinueWith(_ =>
+           {
+               availableTapiLines = tapiManager.Lines;
+               log.InfoFormat("Found {0} lines", availableTapiLines.Length);
+               availableTapiLines.ToList().ForEach(l =>
+               {
+                   log.InfoFormat("Line name {0}, line id {1}", l.Name, l.Id);
+               });
+               checkedListBox1.DataSource = availableTapiLines.Select(l => l.Name).ToArray();
+               labelStatus.Text = "Initialized (select lines and subscribe)";
+               buttonSubscribe.Enabled = true;
+
+           }, CancellationToken.None, TaskContinuationOptions.OnlyOnRanToCompletion,
+                    TaskScheduler.FromCurrentSynchronizationContext());
+
+            taskToInit.ContinueWith(t =>
+            {
+                log.Info("Initialized failed.");
+
+                if (t.Exception != null)
+                    ShowError(t.Exception);
+                else
+                    ShowError();
+
+            }, TaskContinuationOptions.NotOnRanToCompletion);
+        }
+
+        private void TapiManagerOnNewCall(object sender, NewCallEventArgs newCallEventArgs)
+        {
+            lock (lockOnLog)
+            {
+                var call = newCallEventArgs.Call;
+                var line = call.Line;
+                LogWithCallInfo(call, "New call added");
+                LogFullCallInfo(call);
+
+                UpdateList(line, call);
+            }
+        }
+
+        private void TapiManagerOnCallInfoChanged(object sender, CallInfoChangeEventArgs callInfoChangeEventArgs)
+        {
+            lock (lockOnLog)
+            {
+                var call = callInfoChangeEventArgs.Call;
+                var line = call.Line;
+
+                LogWithCallInfo(call, "Call info changed");
+                LogFullCallInfo(call);
+
+                UpdateList(line, call);
+            }
+        }
+
+        private void TapiManagerOnCallStateChanged(object sender, CallStateEventArgs callStateEventArgs)
+        {
+            lock (lockOnLog)
+            {
+                var call = callStateEventArgs.Call;
+                var line = call.Line;
+
+                LogWithCallInfo(call, "Call state changed");
+                LogFullCallInfo(call);
+
+                UpdateList(line, call);
+            }
+        }
+
+        private void UpdateList(TapiLine line, TapiCall call)
+        {
+            Invoke(() =>
+            {
+                var existingItem = listView1.Items.Cast<ListViewItem>().FirstOrDefault(item => item.Tag == line);
+
+                if (existingItem == null)
+                {
+                    var listView = new ListViewItem(line.Name) { Tag = line };
+                    listView.SubItems.AddRange(
+                        new[]
+                        {
+                            call.Id.ToString(),
+                            call.CallState.ToString(),
+                            string.Format("{0} {1}", call.CallerId, call.CallerName),
+                            string.Format("{0} {1}", call.CalledId, call.CalledName),
+                            string.Format("{0} {1}", call.ConnectedId, call.ConnectedName)
+                        });
+                    listView1.Items.Add(listView);
+                }
+                else
+                {
+                    existingItem.SubItems[COLUMNS_STATE].Text = call.CallState.ToString();
+
+                    if (call.CallState == CallState.Idle)
+                    {
+                        call.Dispose();
+                        listView1.Items.Remove(existingItem);
+                    }
+                }
+            });
+        }
+
+        private void Invoke(Action action)
+        {
+            if (InvokeRequired)
+            {
+                BeginInvoke(action);
+                return;
+            }
+            action();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            labelStatus.Text = "Wait while start monitor lines";
+            buttonSubscribe.Enabled = false;
+            buttonUnsubscribe.Enabled = true;
+            checkedListBox1.Enabled = false;
+            checkBox1.Enabled = false;
+
+            var selecedLines = availableTapiLines.Where((t, i) => checkedListBox1.GetItemChecked(i)).ToList();
+            log.InfoFormat("Try to subscribe selected lines {0}", string.Join(", ", selecedLines));
+            var taskToSubscribe = Task.Factory.StartNew(() =>
+            {
+                selecedLines.ForEach(l =>
+                {
+                    log.InfoFormat("Start monitor line {0}", l.ToString());
+                    l.Monitor();
+                });
+                subscribedLines = selecedLines.ToArray();
+            });
+            taskToSubscribe.ContinueWith(_ =>
+            {
+                labelStatus.Text = "Subscribed (all fine!)";
+            }, CancellationToken.None, TaskContinuationOptions.OnlyOnRanToCompletion, TaskScheduler.FromCurrentSynchronizationContext());
+            taskToSubscribe.ContinueWith(t =>
+            {
+                log.ErrorFormat("Error while try to start monitor lines");
+                ShowError(t.Exception);
+            }, TaskContinuationOptions.NotOnRanToCompletion);
+        }
+
+        private void ShowLineTree(TapiLine line, List<TapiLine> excepted)
+        {
+            log.InfoFormat("Tree node {0}", line.Name);
+            excepted.Add(line);
+
+            foreach (var subLine in line.Addresses.Select(l => l.Line))
+            {
+                if (excepted.Contains(subLine))
+                    continue;
+
+                ShowLineTree(subLine, excepted.ToList());
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            labelStatus.Text = "Unsubscribe (whait)";
+            buttonUnsubscribe.Enabled = false;
+
+            var taskToUnsubscribe = Task.Factory.StartNew(() =>
+            {
+                subscribedLines.ToList().ForEach(l =>
+                {
+                    l.Close();
+                });
+            });
+            taskToUnsubscribe.ContinueWith(_ =>
+            {
+                labelStatus.Text = "Initialized (select lines and subscribe)";
+                buttonSubscribe.Enabled = true;
+                checkedListBox1.Enabled = true;
+                checkBox1.Enabled = true;
+            }, CancellationToken.None, TaskContinuationOptions.OnlyOnRanToCompletion,
+                TaskScheduler.FromCurrentSynchronizationContext());
+            taskToUnsubscribe.ContinueWith(t =>
+            {
+                ShowError(t.Exception);
+
+            }, TaskContinuationOptions.NotOnRanToCompletion);
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            for (var i = 0; i < availableTapiLines.Length; i++)
+            {
+                checkedListBox1.SetItemChecked(i, checkBox1.Checked);
+            }
+        }
+
+        private void LogFullCallInfo(TapiCall call)
+        {
+            if (call == null)
+                throw new ArgumentNullException("call");
+
+            LogWithCallInfo(call, string.Format("Info by call {0} and state {1}", call.Id, call.CallState));
+            LogWithCallInfo(call, string.Format("Owner Address {0}", call.Address.Address));
+            LogWithCallInfo(call, string.Format("CalledId {0}, CalledName {1}", call.CalledId ?? string.Empty, call.CalledName ?? string.Empty));
+            LogWithCallInfo(call, string.Format("CallerName {0}, CallerdId {1}", call.CallerId ?? string.Empty, call.CallerId ?? string.Empty));
+            LogWithCallInfo(call, string.Format("ConnectedId {0}, ConnectedName {1}", call.ConnectedId ?? string.Empty, call.ConnectedName ?? string.Empty));
+        }
+
+        private void LogWithCallInfo(TapiCall call, string text)
+        {
+            log.InfoFormat(" {0} {1} " + text, call.Line.Name, call.Id);
+        }
+
+        private void ShowError(Exception exception = null)
+        {
+            Invoke(() =>
+            {
+                labelStatus.Text = "Some error occurred - restart app";
+
+                buttonUnsubscribe.Enabled = false;
+                buttonSubscribe.Enabled = false;
+
+                checkedListBox1.Enabled = false;
+                checkBox1.Enabled = false;
+            });
+
+            if (exception != null)
+                log.Error(exception);
+        }
+    }
+}
